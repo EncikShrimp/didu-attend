73f5c8763307efc3eeb3400f7f5e159559b7ed0a
EncikShrimp <muhammadfaheemwahid@gmail.com>
2025-03-11 12:49:46 +0800

    feat: added auth hooks and protected layout

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import { useRequireAuth } from "@/hooks/useRequireAuth";
+
+interface ProtectedLayoutProps {
+  children: ReactNode;
+}
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  const { isAuthenticated } = useRequireAuth();
+  console.log(isAuthenticated);
+  const router = useRouter();
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated === false) {
+    router.push("/sign-in");
+    return null;
+  }
+
+  return <>{children}</>;
+}
---
+++ app/(protected)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Protected Home</h1>
+      <p>This is your protected root page ("/").</p>
+    </div>
+  );
+}
